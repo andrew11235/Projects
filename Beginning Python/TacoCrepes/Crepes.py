3748af97214b190b494925e4ad1cb26dcc982c9e
Andrew Wang <89869564+p4andrew23@users.noreply.github.com>
2022-04-29 13:29:29 -0700

    Update Crepes.py

--- Beginning Python/TacoCrepes/Crepes.py
+++ Beginning Python/TacoCrepes/Crepes.py
@@ -1,1 +1,15 @@
+from selenium import webdriver
 
+browser = webdriver.Chrome(executable_path='')
+browser.get("https://forms.gle/1vHf6yFeHNGbCv4Q6")
+
+for i in range(100):
+  choice = browser.find_element_by_xpath("")
+  submit = browser.find_element_by_xpath("")
+
+  choice.click()
+  submit.click()
+  
+  browser.refresh()
+  
+browser.close()
