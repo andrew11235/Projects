1b5897b76eef14a9a60586b94618d4c3ebd1b5c1
Andrew Wang <89869564+andrew11235@users.noreply.github.com>
2022-07-04 19:05:27 -0700

    Update main.py

--- Beginning Python/IsThisCheating/main.py
+++ Beginning Python/IsThisCheating/main.py
@@ -1,3 +1,12 @@
+# Andrew Wang 2022
+# 
+# Is This Cheating?
+# Auto-enters guesses into CS10A assessments to brute force crack answers
+# Subverts copy-paste restriction
+# 
+# Credits: Tom Rebold, Monterey Peninsula College
+
+
 import string
 
 from selenium import webdriver
@@ -12,31 +21,20 @@ def input_keys(xpath, keys):
     driver.find_element(by=By.XPATH, value=xpath).send_keys(keys)
 
 
-def input_click(xpath, keys):
-    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, xpath)))
-    driver.find_element(by=By.XPATH, value=xpath).click()
-
-
 driver = webdriver.Chrome()
 driver.get("https://tomrebold.com/csis10a/assess/13/")
 
 input_keys('//*[@id="User"]', "WangAn" + Keys.ENTER)
 
-guess = string.ascii_uppercase + string.ascii_lowercase * 10
-
 WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.NAME, "userAns[]")))
 boxes = driver.find_elements(by=By.NAME, value="userAns[]")
 checks = [driver.find_element(by=By.ID, value=f"Chk{i}") for i in range(1, len(boxes) + 1)]
-submits = driver.find_elements(by=By.NAME, value="SubmitButton")
-submits.pop(0)
 
-# for b in boxes:
-#     b.send_keys(guess)
+# Question Number
+i = 1  
+# Guess string
+guess = string.ascii_uppercase + string.ascii_lowercase * 10
 
-i = 1
 boxes[i - 1].send_keys(guess)
 checks[i - 1].click()
-
-
-
 
